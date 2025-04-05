fa88db74db11d02303d8e0b972f604e7e5eb4f3c
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-05 08:05:33 +0600

    project setup

---
+++ src/main/java/Application/RentalHouse/RentalHouseApplication.java
@@ -0,0 +1,14 @@
+package Application.RentalHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalHouseApplication.class, args);
+		System.out.println("hello my rental house application");
+	}
+
+}
