91de762cff46e6fbcaac5c745f812d77abcbadeb
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-08 07:36:11 +0600

    create user controller for crud operation

---
+++ src/main/java/Application/RentalHouse/Controller/UserCon.java
@@ -0,0 +1,59 @@
+package Application.RentalHouse.Controller;
+
+import Application.RentalHouse.Service.UserService;
+import Application.RentalHouse.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserCon {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    // get user by id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id){
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    //or use
+    //different between responseEntity and optional
+    //if user by id do not have in a database response entity give you not found
+    // and optional give you null value
+
+
+//    @GetMapping("/{id}")
+//    public Optional<User> getuserId(@PathVariable long id){
+//        return userService.getUserById(id);
+//    }
+
+    // create user in db
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUsers(user);
+    }
+
+    //delete user
+    //if user have relationship between another class, then this user you can't delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserId(@PathVariable long id){
+        userService.deleteUserById(id);
+        return ResponseEntity.ok().build();
+    }
+
+//    @DeleteMapping("/{id}")
+//    public void deleteUserById(@PathVariable long id){
+//        userService.deleteUserById(id);
+//    }
+
+}
