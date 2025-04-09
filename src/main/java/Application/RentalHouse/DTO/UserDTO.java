3ee39263c492d9465b1362c42b038ec4d3f71a1d
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-09 07:48:29 +0600

    create user DTO

---
+++ src/main/java/Application/RentalHouse/DTO/UserDTO.java
@@ -0,0 +1,91 @@
+package Application.RentalHouse.DTO;
+
+import Application.RentalHouse.model.Role;
+
+import java.util.Date;
+
+//  Create a custom DTO (Data Transfer Object)
+// that only includes the fields you need.
+// This gives you full control over your API response format.
+public class UserDTO {
+    private long id;
+    private  String name;
+    private String email;
+    private String phone;
+    private String password;
+    private Role role;
+    private Date created_at;
+
+    //constructor
+
+
+    public UserDTO(long id, String name, String email, String phone,
+                   String password, Role role, Date created_at) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.created_at = created_at;
+    }
+
+    // getter setter
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+}
