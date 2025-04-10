90168ec044aaf4ad768e179a1af4b7f3eae6fd68
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-10 07:47:31 +0600

    create get and post

--- src/main/java/Application/RentalHouse/Controller/HouseCon.java
+++ src/main/java/Application/RentalHouse/Controller/HouseCon.java
@@ -1,23 +1,38 @@
 package Application.RentalHouse.Controller;
 
+import Application.RentalHouse.DTO.HouseDTO;
+import Application.RentalHouse.DTO.UserDTO;
 import Application.RentalHouse.Service.HouseService;
+import Application.RentalHouse.Service.HouseServiceIMP;
 import Application.RentalHouse.model.House;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/house")
 public class HouseCon {
     @Autowired
     private HouseService houseService;
+    @GetMapping("/{id}")
+    public ResponseEntity<HouseDTO> getHouse(@PathVariable long id){
+        return ResponseEntity.ok(houseService.getHouse(id));
+    }
+    @PostMapping
+    public ResponseEntity<HouseDTO> createHouse(@RequestBody HouseDTO dto){
+            return  ResponseEntity.status(HttpStatus.CREATED).body(houseService.createHouse(dto));
+    }
+
+
+
+
+
 
     //get houses
-    @GetMapping
-    public List<House> getAllHouse(){
-        return houseService.getAllHouse();
-    }
+//    @GetMapping
+//    public List<House> getAllHouse(){
+//        return houseService.getAllHouse();
+//    }
+
 }
