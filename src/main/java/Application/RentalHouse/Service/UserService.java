1360ccd480a9bbcfa1c977476639c57dea6999f9
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-12 07:53:17 +0600

    remove previous crud operation add new service for best practice code

--- src/main/java/Application/RentalHouse/Service/UserService.java
+++ src/main/java/Application/RentalHouse/Service/UserService.java
@@ -1,5 +1,6 @@
 package Application.RentalHouse.Service;
 
+import Application.RentalHouse.DTO.UserDTO;
 import Application.RentalHouse.Execption.ResourceNotFoundException;
 import Application.RentalHouse.Repository.UsersRepo;
 import Application.RentalHouse.model.User;
@@ -7,41 +8,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-
-@Service
-public class UserService {
-    @Autowired
-    private UsersRepo usersRepo;
 
 
-    //get user
-    public List<User> getAllUsers(){
-        return usersRepo.findAll();
-    }
-    //get user by id
-    public User getUserById(Long id){
-        return usersRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("not found id "+id));
-    }
-    //create user service
-    public User createUsers(User user){
-        return usersRepo.save(user);
-    }
-    //delete data
-    public void deleteUserById(long id){
-        usersRepo.deleteById(id);
-    }
-    //update user by id
-    public User updateUserById(long id, User user){
-        User existingUser = usersRepo.findById(id).
-                orElseThrow(()-> new ResourceNotFoundException("not found id "+id)); ;
+public interface UserService {
 
-            existingUser.setName(user.getName());
-            existingUser.setEmail(user.getEmail());
-            existingUser.setPassword(user.getPassword());
-            existingUser.setPhone(user.getPhone());
-            existingUser.setRole(user.getRole());
-            return usersRepo.save(existingUser);
+    UserDTO createUser(UserDTO userDTO);
+    List<UserDTO> getUser();
 
-    }
+
+
+
+    // we follow best practice that why we can't write business logic in service layer
+    // we write business logic in service implementation layer
+
+//    @Autowired
+//    private UsersRepo usersRepo;
+//
+//
+//    //get user
+//    public List<User> getAllUsers(){
+//        return usersRepo.findAll();
+//    }
+//    //get user by id
+//    public User getUserById(Long id){
+//        return usersRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("not found id "+id));
+//    }
+//    //create user service
+//    public User createUsers(User user){
+//        return usersRepo.save(user);
+//    }
+//    //delete data
+//    public void deleteUserById(long id){
+//        usersRepo.deleteById(id);
+//    }
+//    //update user by id
+//    public User updateUserById(long id, User user){
+//        User existingUser = usersRepo.findById(id).
+//                orElseThrow(()-> new ResourceNotFoundException("not found id "+id)); ;
+//
+//            existingUser.setName(user.getName());
+//            existingUser.setEmail(user.getEmail());
+//            existingUser.setPassword(user.getPassword());
+//            existingUser.setPhone(user.getPhone());
+//            existingUser.setRole(user.getRole());
+//            return usersRepo.save(existingUser);
+
+//    }
 }
