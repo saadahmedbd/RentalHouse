da5c253e886c82b6322617118976208d5b85c496
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-12 07:51:49 +0600

    user mapper setup for mapStruct dto

--- src/main/java/Application/RentalHouse/DTOMapper/UserMapper.java
+++ src/main/java/Application/RentalHouse/DTOMapper/UserMapper.java
@@ -1,33 +1,14 @@
 package Application.RentalHouse.DTOMapper;
 
 import Application.RentalHouse.DTO.UserDTO;
-import Application.RentalHouse.Repository.UsersRepo;
 import Application.RentalHouse.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class UserMapper {
-    @Autowired
-    private UsersRepo usersRepo;
-    public UserDTO toDTO(User user){
-        UserDTO dto =new UserDTO();
-        dto.setId(user.getId());
-        dto.setName(user.getName());
-        dto.setEmail(user.getEmail());
-        dto.setPassword(user.getPassword());
-        dto.setPhone(user.getPhone());
-        dto.setRole(user.getRole());
-        return dto;
-    }
-    public User toEntity(UserDTO dto) {
-        User user = new User();
-        user.setName(dto.getName());
-        user.setEmail(dto.getEmail());
-        user.setPassword(dto.getPassword());
-        user.setPhone(dto.getPhone());
-        user.setRole(dto.getRole());
-        return user;
-    }
+@Mapper(componentModel = "spring")//@Mapper → this is MapStruct mapper interface,
+                                //componentModel =spring it's a use for spring ben
+public interface UserMapper {
+    UserDTO toDTO(User user); // Entity -> DTO
+    User toEntity(UserDTO userDTO); //DTO -> entity
+
 
 }
