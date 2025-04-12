826f35e5c72d73f4a17cb45eb08cfa3967acc803
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-12 07:54:03 +0600

    add user service imp for only busienss logic

---
+++ src/main/java/Application/RentalHouse/Service/Imp/UserServiceIMP.java
@@ -0,0 +1,40 @@
+package Application.RentalHouse.Service.Imp;
+
+import Application.RentalHouse.DTO.UserDTO;
+import Application.RentalHouse.DTOMapper.UserMapper;
+import Application.RentalHouse.Repository.UsersRepo;
+import Application.RentalHouse.Service.UserService;
+import Application.RentalHouse.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceIMP implements UserService {
+    private final UsersRepo usersRepo;
+
+    private final UserMapper userMapper;
+
+    public UserServiceIMP(UsersRepo usersRepo, UserMapper userMapper) {
+        this.usersRepo = usersRepo;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+        User user =userMapper.toEntity(userDTO); //DTO -> entity
+        User saved =usersRepo.save(user);       // save in DB
+        return userMapper.toDTO(saved);         // entity to DTO
+    }
+
+    @Override
+    public List<UserDTO> getUser() {
+        return usersRepo.findAll().stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+
+}
