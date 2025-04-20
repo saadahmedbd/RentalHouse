012e8a9a77a451f1ae76c68cc1663f324bd7ed68
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-20 23:00:24 +0600

    updateHouseDTO for update house data

---
+++ src/main/java/Application/RentalHouse/DTO/UpdateHouseDTO.java
@@ -0,0 +1,89 @@
+package Application.RentalHouse.DTO;
+
+import lombok.Data;
+
+@Data
+public class UpdateHouseDTO {
+    private String title;
+    private String description;
+    private String address;
+    private String city;
+    private double rent_price;
+    private int size;
+    private int bedrooms;
+    private int bathrooms;
+    private boolean available;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getRent_price() {
+        return rent_price;
+    }
+
+    public void setRent_price(double rent_price) {
+        this.rent_price = rent_price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(int bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
+
