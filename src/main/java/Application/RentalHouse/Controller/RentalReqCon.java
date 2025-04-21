23deadf2c90b55c8abc099e6ddba06a2dc82a55a
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-21 16:02:08 +0600

    create and get request logic in controller

---
+++ src/main/java/Application/RentalHouse/Controller/RentalReqCon.java
@@ -0,0 +1,29 @@
+package Application.RentalHouse.Controller;
+
+import Application.RentalHouse.DTO.RentalRequestDTO;
+import Application.RentalHouse.Service.RentalRequestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rentReq")
+public class RentalReqCon {
+    private final RentalRequestService rentalRequestService;
+
+    public RentalReqCon(RentalRequestService rentalRequestService) {
+        this.rentalRequestService = rentalRequestService;
+    }
+    @PostMapping
+    public ResponseEntity<RentalRequestDTO> createRentalReq(@RequestBody RentalRequestDTO rentalRequestDTO){
+//        return ResponseEntity.ok(rentalRequestService.createRentalRequest(rentalRequestDTO));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(rentalRequestService.createRentalReq(rentalRequestDTO));
+    }
+    @GetMapping
+    public List<RentalRequestDTO> getRentalReq(){
+        return rentalRequestService.getRentalReq();
+    }
+}
