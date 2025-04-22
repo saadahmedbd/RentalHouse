4681205006038c65fd9d35e4d4ee9d311e0cf99d
saadahmedbd <saadahmedbd0@gmail.com>
2025-04-22 16:26:49 +0600

    add id, stared and enddate ,but we face pblm (id,stared and enddate set as null) nut we use jackson dependency

--- src/main/java/Application/RentalHouse/DTO/RentalRequestDTO.java
+++ src/main/java/Application/RentalHouse/DTO/RentalRequestDTO.java
@@ -3,13 +3,29 @@ package Application.RentalHouse.DTO;
 import Application.RentalHouse.model.House;
 import Application.RentalHouse.model.Status;
 import Application.RentalHouse.model.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
 
 public class RentalRequestDTO {
-    private long houseId;
-    private long rentalId;
-    private Status status;
+   private long id;
+   private long houseId;
+   private long rentalId;
+   private Status status;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+   private LocalDate  started;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+   private  LocalDate endDate;
 
     public RentalRequestDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public long getHouseId() {
@@ -34,5 +50,21 @@ public class RentalRequestDTO {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public LocalDate getStarted() {
+        return started;
+    }
+
+    public void setStarted(LocalDate started) {
+        this.started = started;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
     }
 }
