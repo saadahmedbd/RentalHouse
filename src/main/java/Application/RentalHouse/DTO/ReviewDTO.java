b44a0ef5b5db1fd81ed38949f9e61cf074cde1b0
saadahmedbd <saadahmedbd0@gmail.com>
2025-06-22 00:01:18 +0600

    add validation on review field

--- src/main/java/Application/RentalHouse/DTO/ReviewDTO.java
+++ src/main/java/Application/RentalHouse/DTO/ReviewDTO.java
@@ -1,6 +1,10 @@
 package Application.RentalHouse.DTO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
@@ -8,7 +12,13 @@ public class ReviewDTO {
     private long id;
     private long houseId;
     private long userId;
+    @NotNull                                    //@NotNull: Ensures the field is not null.
+    @DecimalMin(value = "1.0",inclusive = true, message = "Rating must be at least 1")
+    @DecimalMax(value = "5.0",inclusive = true, message = "Rating must be at most 5")  //inclusive = true:
+    // This means the boundary values 1.0 and 5.0 are allowed.
+    // If you want to exclude them, set inclusive = false.
     private  double rating;
+    @NotBlank
     private String comment;
     private String url;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Dhaka")
